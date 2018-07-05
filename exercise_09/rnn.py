1239b6bbf82ecf542e5ffffbd9db5cdc51420392
machism0 <benmiller1315@gmail.com>
2018-07-05 13:21:39 +0200

    corretly predicted??

--- exercise_09/rnn.py
+++ exercise_09/rnn.py
@@ -7,6 +7,12 @@ from keras.layers import Dense, LSTM, Dropout, Conv1D, MaxPooling1D, BatchNormal
 from keras.callbacks import EarlyStopping
 from keras.optimizers import adam
 
+from numpy.random import seed
+from tensorflow import set_random_seed
+
+seed(42)
+set_random_seed(42)
+
 data_x, data_y, vali_x, vali_y, test_x = utils.load(verbose=True,
                                                     vectorize=True)
 
@@ -18,7 +24,7 @@ count_possible_labels = data_y.shape[1]
 
 model = Sequential()
 model.add(Conv1D(filters=64,
-                 kernel_size=50,
+                 kernel_size=100,
                  padding='same',
                  activation='relu',
                  input_shape=(max_string_length, count_features)))
@@ -26,7 +32,7 @@ model.add(Dropout(0.5))
 model.add(MaxPooling1D(pool_size=2))
 model.add(BatchNormalization())
 model.add(Conv1D(filters=32,
-                 kernel_size=25,
+                 kernel_size=50,
                  padding='same',
                  activation='relu',
                  input_shape=(max_string_length, count_features)))
@@ -42,22 +48,22 @@ model.add(Dense(count_possible_labels,
                 activation='softmax'))
 
 
-opt = adam(lr=.0095,
-           beta_1=0.85,
-           beta_2=0.995)
+opt = adam(lr=.00085,
+           beta_1=0.99,
+           beta_2=0.9999)
 model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
 
-early_stop = EarlyStopping(min_delta=0.01,
-                           patience=5)
+# early_stop = EarlyStopping(min_delta=0.01,
+#                         patience=5)
 print(model.summary())
 model.fit(data_x,
           data_y,
-          epochs=100,
+          epochs=60,
           batch_size=32,
-          validation_data=(vali_x, vali_y),
-          callbacks=early_stop)
+          validation_data=(vali_x, vali_y)) # ,
+#          callbacks=[early_stop])
 
 scores = model.predict_classes(test_x)
 print(scores)
