8f9d54bbadbc19aece8a63bd88e85709c4be71dd
Ben <benmiller1315@gmail.com>
2018-08-22 15:55:16 +0200

    quick and dirty updated to match paper, still dont train

--- contextencoder/contenc.py
+++ contextencoder/contenc.py
@@ -15,26 +15,43 @@ def valid_conv_transpose_math(input_width, filter_width, stride):
 
 
 class ContextEncoder:
-    def __init__(self, batch_tensor, mask, batch_size=100, image_dim=32, filters=48, count_conv=4):
-        self.batch_size = tf.placeholder_with_default(np.int64(batch_size), shape=())
+    def __init__(self, batch_tensor, filters=48, count_conv=4):
+        self.image_batch = batch_tensor
+        self.image_dim = [int(i) for i in self.image_batch.shape[1:]]
+        self.mask_dim = [self.image_dim[0] // 4, self.image_dim[1] // 4, 3]
+        self.border_px = 2
 
-        self.count_conv = count_conv - 1
-        self.filters = filters
-        conv = partial(valid_conv_math, filter_width=3, stride=2)
-        reduced_dim = valid_conv_math(image_dim, filter_width=3, stride=1)
-        for _ in range(self.count_conv):
-            reduced_dim = conv(reduced_dim)
-        self.convd_dim = reduced_dim
-        self.latent_dim = self.filters * self.convd_dim ** 2
+        self.mask = tf.image.resize_image_with_crop_or_pad(
+            tf.ones(shape=self.mask_dim, dtype=tf.bool),
+            self.image_dim[0],
+            self.image_dim[1]
+        )
+        self.over = tf.image.resize_image_with_crop_or_pad(
+            tf.ones(shape=[self.mask_dim[0] + self.border_px, self.mask_dim[1] + self.border_px, 3], dtype=tf.bool),
+            self.image_dim[0],
+            self.image_dim[1]
+        )
+        self.border = self.mask ^ self.over
+
+        self.masked_batch = (
+                tf.cast(~self.mask, dtype=tf.float32) * self.image_batch
+                + 0.5 * tf.cast(self.mask, dtype=tf.float32)
+        )
+        self.cutout_batch = tf.reshape(tf.boolean_mask(self.image_batch, self.mask, axis=1), shape=[-1] + self.mask_dim)
 
         self.is_training = tf.placeholder_with_default(True, shape=())
-        self.image_batch = batch_tensor
-        self.mask = mask
-        mask_length = int(np.sqrt(np.sum(self.mask) // 3))
-        self.mask_dim = [-1, mask_length, mask_length, 3]
-        self.masked_batch = (~self.mask) * self.image_batch
-        self.cutout = tf.reshape(tf.boolean_mask(self.image_batch, self.mask, axis=1),
-                                 shape=self.mask_dim)
+        self.count_conv = count_conv - 1
+        self.filters = filters
+
+        def calculate_reduced_dim(image_dim):
+            conv = partial(valid_conv_math, filter_width=3, stride=2)
+            reduced_dim = valid_conv_math(image_dim, filter_width=3, stride=1)
+            for _ in range(self.count_conv):
+                reduced_dim = conv(reduced_dim)
+            return reduced_dim
+
+        self.convd_dim = calculate_reduced_dim(self.image_dim[0])
+        self.latent_dim = self.filters * self.convd_dim ** 2
 
         self.z = self._encoder()
         self.decoded = self._decoder()
@@ -43,9 +60,18 @@ class ContextEncoder:
             int(self.image_batch.shape[1]),
             int(self.image_batch.shape[2])
         )
+        self.output_batch = tf.reshape(
+            tf.boolean_mask(self.padded_decoded, self.mask, axis=1),
+            shape=[-1] + self.mask_dim
+        )
+        self.output_padded = tf.image.resize_image_with_crop_or_pad(
+            self.output_batch,
+            self.image_dim[0],
+            self.image_dim[1]
+        )
 
-        self.d_fake = self._discriminator(self.decoded, reuse=False, is_training=False)
-        self.d_real = self._discriminator(self.cutout, reuse=True, is_training=True)
+        self.d_fake = self._discriminator(self.output_batch, reuse=False, is_training=False)
+        self.d_real = self._discriminator(self.cutout_batch, reuse=True, is_training=True)
 
         self.loss, self.optimization, self.reconstruction_loss, self.adversarial_loss = self._context_encoder_loss()
         self.d_loss, self.d_opt = self._discriminator_loss()
@@ -70,7 +96,7 @@ class ContextEncoder:
                 conv_kwargs = {'kernel_size': 3, 'strides': 1, 'padding': 'valid', 'activation': tf.nn.relu}
                 x = tf.layers.dense(self.z, units=self.latent_dim, activation=tf.nn.relu)
                 x = tf.reshape(x, shape=[-1, self.convd_dim, self.convd_dim, self.filters])
-                for i in range(self.count_conv):
+                for i in range(self.count_conv + 1):
                     count_filters = self.filters // (2 ** i)
                     x = tf.layers.conv2d_transpose(x, filters=count_filters, **conv_kwargs)
                 decoded = tf.layers.conv2d(x, kernel_size=3, filters=3, padding='same', activation=tf.nn.sigmoid)
@@ -103,11 +129,23 @@ class ContextEncoder:
         return x
 
     def _context_encoder_loss(self, reconstruct_coef=0.999):
-        # mask is 1s when dropped, 0s when not dropped
+
         reconstruction_loss = tf.reduce_sum(
-            tf.squared_difference(self.cutout, self.decoded),
+            tf.squared_difference(
+                tf.boolean_mask(self.image_batch, self.mask, axis=1),
+                tf.boolean_mask(self.padded_decoded, self.mask, axis=1)
+            ),
             axis=[1]
         )
+
+        reconstruction_loss += 10 * tf.reduce_sum(
+            tf.squared_difference(
+                tf.boolean_mask(self.image_batch, self.border, axis=1),
+                tf.boolean_mask(self.padded_decoded, self.border, axis=1)
+            ),
+            axis=[1]
+        )
+
         reconstruction_loss = tf.reduce_mean(reconstruction_loss)
 
         adversarial_loss = -tf.reduce_mean(
@@ -118,7 +156,8 @@ class ContextEncoder:
         loss = reconstruct_coef * reconstruction_loss + (1 - reconstruct_coef) * adversarial_loss
 
         opt = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(
-            loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='context'),
+            loss,
+            var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='context'),
             name='context_encoder_solver'
         )
         return loss, opt, reconstruction_loss, adversarial_loss
@@ -129,7 +168,7 @@ class ContextEncoder:
             name='discriminator_loss'
         )
 
-        opt = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(
+        opt = tf.train.AdamOptimizer(learning_rate=1e-6).minimize(
             discriminator_loss,
             var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator'),
             name='discriminator_solver'
@@ -150,7 +189,7 @@ class ContextEncoder:
     def compute_batch(self, session, feed_dict):
         feed_dict[self.is_training] = False
         image, masked, decoded = session.run(
-            [self.image_batch, self.masked_batch, self.padded_decoded],
+            [self.image_batch, self.masked_batch, self.output_padded],
             feed_dict=feed_dict
         )
         return image, masked, decoded
@@ -177,7 +216,7 @@ def main():
     iterator = tf.data.Iterator.from_string_handle(handle, train_dataset.output_types, train_dataset.output_shapes)
     image, label = iterator.get_next()
 
-    ce = ContextEncoder(image, mask)
+    ce = ContextEncoder(image)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
