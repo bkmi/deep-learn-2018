46c4a22080dc81aee7587610420e5791bcd5138c
machism0 <benmiller1315@gmail.com>
2018-10-05 22:35:35 +0200

    fixed save fails of training script

--- dcgan/traineach.sh
+++ dcgan/traineach.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-EPOCHS=1
+EPOCHS=5000
 SAVE=true
 python train.py --gantype dcgan --savepics $SAVE --savemodels $SAVE --epochs $EPOCHS
 mv save save_dcgan
 python train.py --gantype wgan --savepics $SAVE --savemodels $SAVE --epochs $EPOCHS
-mv save save_dcgan
+mv save save_wgan
 python train.py --gantype iwgan --savepics $SAVE --savemodels $SAVE --epochs $EPOCHS
-mv save save_dcgan
+mv save save_iwgan
