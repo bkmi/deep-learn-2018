45dd76b5c39bf7a8a490fe3f87d672ddb1191f91
machism0 <machism0@users.noreply.github.com>
2018-10-12 11:31:28 +0200

    added batch norm setting

--- dcgan/network.py
+++ dcgan/network.py
@@ -40,8 +40,17 @@ def lazy_property(func):
 
 
 class DCGAN:
-    def __init__(self, real_batch, latent_batch, learning_rate=0.0002, beta1=0.5, beta2=0.9, log_bias=1e-12):
+    def __init__(self,
+                 real_batch,
+                 latent_batch,
+                 learning_rate=0.0002,
+                 beta1=0.5,
+                 beta2=0.9,
+                 log_bias=1e-12,
+                 use_batch_norm=True
+                 ):
         self.is_training = tf.placeholder_with_default(True, shape=())
+        self._use_batch_norm = use_batch_norm
         self.log_bias = log_bias
 
         self.real_batch = real_batch
@@ -154,14 +163,18 @@ class MNISTDCGAN(DCGAN):
         super().__init__(*args, **kwargs)
 
     def _generator(self, latent):
+        conv_kwargs = {'kernel_size': 5, 'strides': 2, 'activation': tf.nn.relu}
         with tf.variable_scope(name_or_scope="generator"):
             x = tf.layers.dense(latent, 3 * 3 * self.filters_max, activation=None)
-            x = tf.layers.batch_normalization(x, training=self.is_training)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, training=self.is_training)
             x = tf.reshape(x, shape=(-1, 3, 3, self.filters_max))  # 3, 3
             x = tf.layers.conv2d_transpose(x, filters=self.filters_max // 2, kernel_size=3, strides=2, activation=tf.nn.relu)  # 7, 7
-            x = tf.layers.batch_normalization(x, training=self.is_training)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, training=self.is_training)
             x = tf.layers.conv2d_transpose(x, filters=self.filters_max // 4, kernel_size=2, strides=2, activation=tf.nn.relu)  # 14, 14
-            x = tf.layers.batch_normalization(x, training=self.is_training)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, training=self.is_training)
             x = tf.layers.conv2d_transpose(x, filters=1, kernel_size=2, strides=2, activation=tf.nn.tanh)  # 28, 28
         return x
 
@@ -170,11 +183,14 @@ class MNISTDCGAN(DCGAN):
         bnorm_kwargs = {'training': self.is_training, 'reuse': reuse}
         with tf.variable_scope(name_or_scope="discriminator", reuse=reuse):
             x = tf.layers.conv2d(image, filters=self.filters_max // 4, name='c0', **conv_kwargs)  # 14, 14
-            x = tf.layers.batch_normalization(x, name='b0', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b0', **bnorm_kwargs)
             x = tf.layers.conv2d(x, filters=self.filters_max // 2, name='c1', **conv_kwargs)  # 7, 7
-            x = tf.layers.batch_normalization(x, name='b1', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b1', **bnorm_kwargs)
             x = tf.layers.conv2d(x, filters=self.filters_max, name='c2', **conv_kwargs)  # 3, 3
-            x = tf.layers.batch_normalization(x, name='b2', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b2', **bnorm_kwargs)
             x = tf.layers.flatten(x, name='flat')
             x = tf.layers.dense(x, 1, activation=tf.nn.sigmoid, name='d0', reuse=reuse)
             # x = tf.layers.batch_normalization(x, name='b3', **bnorm_kwargs)
@@ -188,12 +204,15 @@ class CIFARDCGAN(DCGAN):
     def _generator(self, latent):
         with tf.variable_scope(name_or_scope="generator"):
             x = tf.layers.dense(latent, 4 * 4 * 512, activation=None)
-            x = tf.layers.batch_normalization(x, training=self.is_training)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, training=self.is_training)
             x = tf.reshape(x, shape=(-1, 4, 4, 512))  # 4, 4
             x = tf.layers.conv2d_transpose(x, filters=256, kernel_size=3, strides=2, activation=tf.nn.relu)  # 7, 7
-            x = tf.layers.batch_normalization(x, training=self.is_training)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, training=self.is_training)
             x = tf.layers.conv2d_transpose(x, filters=128, kernel_size=2, strides=2, activation=tf.nn.relu)  # 14, 14
-            x = tf.layers.batch_normalization(x, training=self.is_training)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, training=self.is_training)
             x = tf.layers.conv2d_transpose(x, filters=1, kernel_size=2, strides=2, activation=tf.nn.tanh)  # 28, 28
         return x
 
@@ -202,14 +221,18 @@ class CIFARDCGAN(DCGAN):
         bnorm_kwargs = {'training': self.is_training, 'reuse': reuse}
         with tf.variable_scope(name_or_scope="discriminator", reuse=reuse):
             x = tf.layers.conv2d(image, filters=128, name='c0', **conv_kwargs)  # 14, 14
-            x = tf.layers.batch_normalization(x, name='b0', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b0', **bnorm_kwargs)
             x = tf.layers.conv2d(x, filters=256, name='c1', **conv_kwargs)  # 7, 7
-            x = tf.layers.batch_normalization(x, name='b1', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b1', **bnorm_kwargs)
             x = tf.layers.conv2d(x, filters=512, name='c2', **conv_kwargs)  # 3, 3
-            x = tf.layers.batch_normalization(x, name='b2', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b2', **bnorm_kwargs)
             x = tf.layers.flatten(x, name='flat')
             x = tf.layers.dense(x, 1, activation=tf.nn.sigmoid, name='d0', reuse=reuse)
-            x = tf.layers.batch_normalization(x, name='b3', **bnorm_kwargs)
+            if self._use_batch_norm:
+                x = tf.layers.batch_normalization(x, name='b3', **bnorm_kwargs)
         return x
 
 
